142fc842f1ed3eae64aa359b24c72880c204ac32
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2025-03-05 21:15:40 +0530

    exploring get, post, delete method

--- src/app.js
+++ src/app.js
@@ -2,6 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+    res.send({ first_name: "tushar", last_name: "solanki" });
+})
+
+app.post("/user", (req, res) => {
+    res.send("Data successfully saved to the database");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully !")
+})
+
+
+
+
+
+//this will match all the HTTP method API calls to /test
 app.use("/hello/2", (req, res) => {
     res.send('hello 2')
 });
@@ -13,3 +30,12 @@ app.use("/hello", (req, res) => {
 app.use("/test", (req, res) => {
     res.send("test")
 });
+
+app.use("/", (req, res) => {
+    res.send("namaste tushar")
+})
+
+app.listen(4444, () => {
+    console.log('Server is successfully listening on port 7777');
+});
+
